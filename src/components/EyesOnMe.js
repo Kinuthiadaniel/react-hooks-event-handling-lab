23ee5afa5b4831808277f6ee0324ec3dbdc2a56e
Kinuthiadaniel <dankinuthia17@gmail.com>
2024-04-25 14:28:32 +0300

    completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+  function onFocus(event){
+    console.log("Good!")
+  }
+  function onBlur(event){
+    console.log("Hey! Eyes on me!")
+  }
+  return (
+    <>
+    <button onFocus={onFocus} onBlur={onBlur}>Eyes on me</button>
+    </>
+  )
+}
+
+export default EyesOnMe;
